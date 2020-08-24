2e5a910f0a6a9f4a8e39e2ee5c8582d843278fa7
usha2405 <usha.jain@somaiya.edu>
2020-08-24 15:10:45 +0530

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+def palindrome(str):
+    string=str.replace(" ","")
+    reverse=string[::-1]
+    if reverse==string:
+        print(f"{str} is a palindrome.")
+    else:
+        print(f"{str} is not a palindrome")
+
+str=input("Enter a string: ")
+palindrome(str)
